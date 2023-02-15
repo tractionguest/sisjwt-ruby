a57a465e08cc7bb43cc2d119f856169d164223ea
Jon Sangster <jon.sangster@tractionguest.com>
2023-02-14 16:38:18 -0800

    Create RSpec support directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
+
 require 'simplecov'
 SimpleCov.start
 
 require "sisjwt"
+Dir[Pathname.new(__dir__).join('support', '**', '*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -14,27 +16,4 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
-
-  config.around(:example, :env) do |example|
-    env_meta = Array(example.metadata[:env])
-    orig_values = {}
-
-    env_meta.each do |entry|
-      key,value = entry.split("=", 2)
-
-      orig_values[key] = ENV[key]
-      if value.nil?
-        ENV.delete(key)
-      else
-        ENV[key] = value
-      end
-    end
-
-    example.run
-
-    orig_values.each do |key, orig_value|
-      ENV[key] = orig_value
-    end
-  end
-
 end
---
+++ spec/support/env.rb
@@ -0,0 +1,23 @@
+RSpec.configure do |config|
+  config.around(:example, :env) do |example|
+    env_meta = Array(example.metadata[:env])
+    orig_values = {}
+
+    env_meta.each do |entry|
+      key, value = entry.split('=', 2)
+
+      orig_values[key] = ENV[key]
+      if value.nil?
+        ENV.delete(key)
+      else
+        ENV[key] = value
+      end
+    end
+
+    example.run
+
+    orig_values.each do |key, orig_value|
+      ENV[key] = orig_value
+    end
+  end
+end
